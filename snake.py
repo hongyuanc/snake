f0f7054c210f8cc9ae5409fb3b7668a492ac2ba3
Hong <hong.cao0824@gmail.com>
2024-02-24 16:10:57 -0500

    set up relevant functions and classes

--- snake.py
+++ snake.py
@@ -3,3 +3,23 @@
 from tkinter import *
 import random
 
+class snake:
+    pass
+
+class food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(direction):
+    pass
+
+def collision():
+    pass
+
+def game_over():
+    pass
+
+def restart_game():
+    pass
