82de0e62c840b572c046f2498dec8da2f6bfd93c
Hong <hong.cao0824@gmail.com>
2024-02-24 22:24:43 -0500

    modifications to the display of the game board

--- snake.py
+++ snake.py
@@ -4,11 +4,15 @@ from tkinter import *
 import random
 
 # game values
-GAME_HEIGHT = 1000
-GAME_WIDTH = 1000
+GAME_HEIGHT = '10i'
+GAME_WIDTH = '10i'
 SPEED = 50
 SPACE_SIZE = 100
 BODY_PARTS = 3
+BACKGROUND = '#FFFFFF'
+BODY_COLOR = '#6495ED'
+FOOD_COLOR = '#B53737'
+
 class snake:
     pass
 
@@ -38,6 +42,25 @@ window.resizable(False, False)
 score = 0
 direction = 'down'
 
-label = Label(window, text='Score:{}'.format(score))
+# displays score
+label = Label(window, text='Score:{}'.format(score), font = ('consolas', 40))
+label.pack()
+
+# displays the game board
+canvas = Canvas(window, bg=BACKGROUND, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+# starts the console in the middle of the screen
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 window.mainloop()
