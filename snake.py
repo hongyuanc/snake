20186d844cdbe4edbd7d840d1b9f5c4b6ef4fe1a
Hong <hong.cao0824@gmail.com>
2024-02-25 21:52:56 -0500

    finished next_turn - might come back and add later

--- snake.py
+++ snake.py
@@ -53,6 +53,22 @@ def next_turn(snake, food):
     else:
         x -= SPACE_SIZE
 
+    snake.coordinates.insert(0,(x,y))
+
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=BODY_COLOR)
+
+    snake.squares.insert(0, square)
+
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+        score += 1
+        label.config(text="Score:{}".format(score))
+        canvas.delete("food")
+        food = food()
+    else:
+        del snake.coordinates[-1]
+        canvas.delete(snake.squares[-1])
+        del snake.squares[-1]
+
 def change_direction(direction):
     pass
 
